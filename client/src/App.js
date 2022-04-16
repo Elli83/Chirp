3c0a4b91b143dfed207649f50724314c102d915c
Elliot <elliothills42@gmail.com>
2022-04-16 23:06:10 +0100

    added empty pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
-import "./App.css"
+import React from "react"
+import "./app.scss"
+
+// COMPONENTS
+import Navbar from "./components/navbar/Navbar"
+
+// PAGES
+import Login from "./pages/login/Login"
+import Home from "./pages/home/Home"
 
 function App() {
-	return <div></div>
+	return (
+		<React.Fragment>
+			<Login />
+		</React.Fragment>
+	)
 }
 
 export default App
