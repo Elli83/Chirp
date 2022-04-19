2603a9fb37fa701a0b48ba3c5418dbbe73b55740
Elliot <elliothills42@gmail.com>
2022-04-19 15:05:27 +0100

    added more user info

--- client/src/pages/profile/Profile.js
+++ client/src/pages/profile/Profile.js
@@ -12,6 +12,24 @@ export default function Profile() {
 						<div id='photo'></div>
 						<span id='name'>John Doe</span>
 						<span id='username'>@johndoe123</span>
+						<span id='bio'>
+							Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+						</span>
+
+						<div id='stats'>
+							<div className='stat'>
+								<span className='title'>Posts</span>
+								<span className='number'>123</span>
+							</div>
+							<div className='stat'>
+								<span className='title'>Followers</span>
+								<span className='number'>789</span>
+							</div>
+							<div className='stat'>
+								<span className='title'>Following</span>
+								<span className='number'>456</span>
+							</div>
+						</div>
 					</div>
 				</div>
 				<div id='posts'>
